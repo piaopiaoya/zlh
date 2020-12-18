c277d12910fa64618e22f20f552d3b9a57648d5f
piaopiaoya <735602390@qq.com>
2020-12-18 09:11:09 +0800

    继续提交

--- src/main/java/com/example/text_01/fragment/CollFragment.java
+++ src/main/java/com/example/text_01/fragment/CollFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -55,7 +56,7 @@ public class CollFragment extends Fragment implements MainContacrt.getNumView {
 
     @Override
     public void NumNO(String err) {
-
+        Toast.makeText(getActivity(), "num错误信息", Toast.LENGTH_SHORT).show();
     }
 }
 
